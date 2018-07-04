b962038ed357ac660aba153ef33797b923f4e2ea
Albert Pedrola <b.albemar@gmail.com>
2018-07-04 10:20:08 +0200

    created components'

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
       <h1>Adobe Practice Barcelona Guidelines</h1>
       <div class="menu">
         <ul>
-          <a href=""><li>Outlook</li></a>
-          <a href=""><li>Teams</li></a>
-          <a href=""><li class="last">Holidays</li></a>
+          <a class="menu-list" href=""><li>Outlook</li></a>
+          <a class="menu-list" href=""><li>Teams</li></a>
+          <a class="menu-list menu-list_last" href=""><li>Holidays</li></a>
         </ul>
       </div>
     </header>
@@ -19,11 +19,15 @@
 </template>
 
 <script>
+import outlook from './components/outlook.vue'
+import teams from './components/teams.vue'
+import holidays from './components/holidays.vue'
+
 export default {
   name: 'app',
   data () {
     return {
-      entries: []
+      
     }
   }
 }
@@ -39,29 +43,49 @@ body {
 
   header {
     color: #ebebeb;
-    background-color: #2d2d2d;
+    background-color: #1a4b77;
 
     h1 {
+      padding: 20px;
       width: 50%;
+      font-size: 20px;
       display: inline;
+      margin-left: 50px;
     }
 
     .menu {
       width: 50%;
       display: inline-block;
+      font-size: 20px;
+      text-align: right;
+      padding: 20px;
+      margin-left: 80px;
+
+      ul {
+        margin: 0;
+      }
 
       a {
+        padding: 20px;
         text-decoration: none;
         color: #ebebeb;
+        transition: 0.6s;
+
+        &:hover {
+          background-color: #2A76BA;
+        }
+
+        .menu-list {
+          display: inline-block;
+          margin-right: 20px;
+        }
+        .menu-list_last {
+          margin-right: 0;
+        }
 
         li {
           display: inline-block;
-          margin-right: 20px;
-
-          &.last {
-            margin-right: 0;
-          }
-        }
+        }      
       }
     }
   }
